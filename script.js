80f7a1a17505ee8d4bc57b86825722973ef9e075
JmYn14091983 <jnahmias83@gmail.com>
2022-10-31 18:46:52 +0200

    last version

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 function zip(str) {
-    let letters = [];
-    letters[0] = str[0];
-  
-    for (let i = 1; i < str.length; i++) {
-      if (str[i] != str[i - 1]) letters.push(str[i]);
-    }
-    return letters;
-  }
-  
-  console.log(zip("AACCCATGGTTGG"));
-  
-  function zipAdvanced(str) {
-    let count = 1;
-    let letters; 
-    letters[0]= [[`'${str[0]}',${count}`]];
+  let letters = [];
+  letters[0] = str[0];
 
-    for (let i = 1; i < str.length; i++) {
-      let count = 1;
-      let index = 1;
-      if (str[i] == str[i-1]) {
-        count++;
-        letters[index] = [`'${str[i]}',${count}`];
-      } 
-      else if (str[i] != str[i-1]) {
-        index++;
-        count = 1
-        letters[index] = [[`'${str[i}',${count}`]];
-      }
-    }
-    return letters;
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] != str[i - 1]) letters.push(str[i]);
   }
-  
-  console.log(zipAdvanced("AACCCATGGTTGG"));
-  
+  return letters;
+}
+
+console.log(zip("AACCCATGGTTGG"));
+
+function zipAdvanced(str) {
+  let letters = [];
+  let count = 1;
+  let index = 0;
+
+  letters[index] = [[`'${str[0]}',${count}`]];
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] == str[i - 1]) count++;
+    else if (str[i] != str[i - 1]) {
+      index++;
+      count = 1;
+    }
+
+    letters[index] = [`'${str[i]}',${count}`];
+  }
+
+  return letters;
+}
+
+console.log(zipAdvanced("ATCCCATGGTTGGGGG"));
